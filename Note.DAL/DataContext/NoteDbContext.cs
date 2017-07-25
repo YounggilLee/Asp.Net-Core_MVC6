b929dfbc10c186f420402ff8be2a4b05cd6763e5
Younggil Lee <leyoungg@sheridancollege.ca>
2017-07-25 16:37:19 -0400

    Set Database connection

--- Note.DAL/DataContext/NoteDbContext.cs
+++ Note.DAL/DataContext/NoteDbContext.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Note.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Note.DAL.DataContext
 {
-    class NoteDbContext
+    public class NoteDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public NoteDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Notice> Notices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=NoteDb;");
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("LocalDb"));
+        }
     }
 }
--- Note.MVC6/Startup.cs
+++ Note.MVC6/Startup.cs
@@ -30,6 +30,8 @@ namespace Note.MVC6
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfiguration>(Configuration);
+
             // Add framework services.
             services.AddTransient<NoticeBll>();
             services.AddTransient<INoticeDal, NoticeDal>();
