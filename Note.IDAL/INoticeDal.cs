8f66c5feedfb22eba6458367ca8cc40e081a05c4
Younggil Lee <leyoungg@sheridancollege.ca>
2017-07-25 16:20:05 -0400

    Setting project for creating Note using Enterprise Architecture

---
+++ Note.IDAL/INoticeDal.cs
@@ -0,0 +1,20 @@
+﻿using Note.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Note.IDAL
+{
+    public interface INoticeDal
+    {
+        List<Notice> GetNoticeList();
+
+        Notice GetNotice(int noticeNo);
+
+        bool PostNotice(Notice notice);
+
+        bool UpdateNotice(Notice notice);
+
+        bool DeleteNotice(int noticeNo);
+
+    }
+}
---
+++ Note.Model/Notice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Note.Model
+{
+    public class Notice
+    {
+        [Key]   //primary key
+        public int NoticeNo { get; set; }
+
+        [Required]  //not null
+        public string NoticeTitle { get; set; }
+
+        [Required]
+        public string NoticeContents { get; set; }
+
+        [Required]
+        public DateTime NoticeWriteDate { get; set; }
+
+    }
+}
